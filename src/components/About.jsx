7a654afeb2a2ca403c928ab9399bf64501ea3ebd
amandal0862 <amandal.eduu@gmail.com>
2022-12-16 18:15:36 +0530

    Added contact Us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
       name="About"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen p-4 flex flex-col justify-center w-full h-full mx-auto">
+      <div className="max-w-screen-lg p-4 flex flex-col justify-center w-full h-full mx-auto">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
