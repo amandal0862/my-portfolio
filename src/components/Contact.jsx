24de681e4f22a4e64513af6217a3f937305cc2b8
amandal0862 <amandal.eduu@gmail.com>
2022-12-26 21:37:52 +0530

    Fixed contact us position

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,10 +4,10 @@ const Contact = () => {
   return (
     <div
       name="Contact"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black p-4 text-white"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black pt-8 text-white"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-        <div className="pb-8">
+        <div className="pb-5">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
           </p>
@@ -35,7 +35,7 @@ const Contact = () => {
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
 
-            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex item-center rounded-md hover:scale-110 duration-300">
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 mt-8 mx-auto flex item-center rounded-md hover:scale-110 duration-300">
               Let's talk
             </button>
           </form>
